0f0621f6be795c2d2f7f8fabf504bd017495b129
Liam <liam@scalzulli.com>
2024-06-09 17:13:57 -0400

    Add shacdn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Button } from "./components/ui/button"
+
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return <div><h1 className='text-3xl font-bold underline'>Hello world!</h1><Button>This is a button</Button></div>
 };
 
 export default App;
